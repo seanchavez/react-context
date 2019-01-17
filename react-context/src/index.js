8c9865e89859da3f01ad2217d4bf290049633c11
Sean Chavez <realseanchavez@gmail.com>
2019-01-16 22:00:11 -0800

    refactor login logic into UserContext

--- react-context/src/index.js
+++ react-context/src/index.js
@@ -2,39 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginPage from './LoginPage';
 import MainPage from './MainPage';
-import UserContext from './UserContext';
+import { UserProvider, UserConsumer } from './UserContext';
 import './index.css';
 
-class Root extends React.Component {
-  state = {
-    currentUser: null,
-  };
-
-  handleLogin = user => {
-    this.setState({ currentUser: user });
-  };
-
-  handleLogout = () => {
-    this.setState({ currentUser: null });
-  };
-
-  render() {
-    return (
-      <UserContext.Provider
-        value={{
-          user: this.state.currentUser,
-          onLogin: this.handleLogin,
-          onLogout: this.handleLogOut,
-        }}
-      >
-        {this.state.currentUser ? (
-          <MainPage />
-        ) : (
-          <LoginPage onLogin={this.handleLogin} />
-        )}
-      </UserContext.Provider>
-    );
-  }
-}
+const Root = () => {
+  return (
+    <UserProvider>
+      <UserConsumer>
+        {({ user }) => (user ? <MainPage /> : <LoginPage />)}
+      </UserConsumer>
+    </UserProvider>
+  );
+};
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
