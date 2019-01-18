a23c3a728972ddb65cc513d1ab664b157592ceb9
Sean Chavez <realseanchavez@gmail.com>
2019-01-17 18:34:32 -0800

    fix css and EmailContext; finish MessageList

--- react-context/src/MessageList.js
+++ react-context/src/MessageList.js
@@ -8,14 +8,35 @@ const MessageList = () => (
       <EmailConsumer>
         {({ loading, emails, onSelectEmail }) => (
           <div className='MessageList'>
-            <div className='no-messages'>
-              Your mailbox is empty, {user.firstName}! 🎉
-            </div>
+            {loading ? (
+              <div className='no-messages'>Loading...</div>
+            ) : emails.length === 0 ? (
+              <div className='no-messages'>
+                Your mailbox is empty, {user.firstName}! 🎉
+              </div>
+            ) : (
+              <ul>
+                {emails.map(email => (
+                  <Email
+                    key={email.id}
+                    email={email}
+                    onClick={() => onSelectEmail(email)}
+                  />
+                ))}
+              </ul>
+            )}
           </div>
         )}
       </EmailConsumer>
     )}
   </UserConsumer>
+);
+
+const Email = ({ email, onClick }) => (
+  <li onClick={onClick}>
+    <div className='subject'>{email.subject}</div>
+    <div className='preview'>{email.preview}</div>
+  </li>
 );
 
 export default MessageList;
--- react-context/src/api.js
+++ react-context/src/api.js
@@ -3,7 +3,7 @@ export const FAKE_USER = {
   lastName: 'Ceddia',
   username: 'dave',
   avatar:
-    'https://www.gravatar.com/avatar/5c3dd2d257ff0e14dbd2583485dbd44b?s=32'
+    'https://www.gravatar.com/avatar/5c3dd2d257ff0e14dbd2583485dbd44b?s=32',
 };
 
 export function login(username, password) {
@@ -14,6 +14,36 @@ export function login(username, password) {
       } else {
         reject({ message: 'Invalid username or password' });
       }
+    }, 300);
+  });
+}
+
+export const FAKE_EMAILS = [
+  {
+    id: 0,
+    subject: 'Hey Dave',
+    body: 'Yo, just wanted to say hey.',
+  },
+  {
+    id: 1,
+    subject: 'React is great',
+    body: 'I thought I should let you know.',
+  },
+  {
+    id: 2,
+    subject: 'REQUEST FOR ASSISTANCE',
+    body:
+      'If you send me your bank account number I will reward you with $10 million whole US dollars.',
+  },
+];
+
+// Generate a preview
+FAKE_EMAILS.forEach(email => (email.preview = email.body.substr(0, 46)));
+
+export function fetchEmails() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(FAKE_EMAILS);
     }, 300);
   });
 }
